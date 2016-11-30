3fd988987f8e867a4b59f806a1b0868f290f9a24
wwylele <wwylele@gmail.com>
2016-11-30 11:32:09 +0200

    Config: auto-select region and language

--- src/core/settings.h
+++ src/core/settings.h
@@ -109,5 +109,9 @@ struct Values {
     u16 gdbstub_port;
 } extern values;
 
+// a special value for Values::region_value indicating that citra will automatically select a region
+// value to fit the region lockout info of the game
+constexpr int REGION_VALUE_AUTO_SELECT = -1;
+
 void Apply();
 }
